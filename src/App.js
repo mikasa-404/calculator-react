cc2452155d9182aa45f8921953997c90ffc1b268
mikasa404 <jhapriya106@gmail.com>
2022-12-05 21:40:05 +0530

    added use Reducer hook and actions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { useReducer } from "react"
 import "./App.css"
+
+const ACTIONS={
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate",
+}
+
+function reducer(state, {type, payload}){  
+  //actions is broken into->{type, payload}, different type of actions 
+
+}
+
 function App(){
+  //state is -> {currentOpearand, previousOperand, operation} 
+  const[{currentOperand, previousOperand, operation}, dispatch]=useReducer(reducer, {}) //default state {}}
   return(
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
